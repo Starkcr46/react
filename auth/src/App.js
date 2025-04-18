b309d1870f2dcb63ee4f8509872f2fc5b74b633a
Starkcr46 <starkcr39@gmail.com>
2025-04-18 11:15:26 -0500

    auth

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,13 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
 import './App.css';
 import Dashboard from './Dashboard';
 import Login from './Login';
 import Preferences from './Preferences';
+import useToken from './useToken';
+
+function setToken(userToken) {
+  sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
+function getToken() {
+  const tokenString = sessionStorage.getItem('token');
+  const userToken = JSON.parse(tokenString);
+  return userToken?.token
+}
 
 function App() {
-  const [token, setToken] = useState();
+  const { token, setToken } = useToken();
 
   if(!token) {
     return <Login setToken={setToken} />
